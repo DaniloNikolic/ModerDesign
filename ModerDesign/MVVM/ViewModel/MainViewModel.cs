b7e7d0e743b1bd7981007769841f1508ff4f82a3
Danilo Nikolic <danilo@dramin-ns.com>
2021-07-04 13:11:52 +0200

    20:29

---
+++ ModerDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ModerDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModerDesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
